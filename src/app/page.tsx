a498d8eb85a49e509552a1f17fc19b44c88c6291
Johthema <mauriciojodi@gmail.com>
2023-06-12 17:47:51 -0400

    Atualiza versao header menus em andamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from '../pages/home/index'
+import './globals.css'
 // import CadastroUsuario  from '../pages/cadastroUsuario'
 // import cadastrarUsuario from '../pages/usuarios/cadastrarUsuario/index'
 // import CadastrarUsuario from '../pages/usuarios/cadastrarUsuario/index'
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,26 +1,33 @@
 import Link from 'next/link'
+import '../../app/globals.css'
 //import estyleBar from '../../css/header.module.css'
-import estyleBar from '../header/heade.module.css'
+import styleBar from '../header/heade.module.css'
 
 
 export default function Header(){
     return(
      
       <>
-      <div className={estyleBar.headerBack}>
-        <div className={estyleBar.opcoesMenu}>
+      <div className={styleBar.headerBack}>
+        <div className={styleBar.divlogotipo}>
+          <h3>Logotipo</h3>
+        </div>
+        <div className={styleBar.opcoesMenu}>
      
-    
-      <Link href="/plantar"><h2>Plantar</h2></Link>
-      <Link href="/home"><h2>Árvores</h2></Link>
-      <Link href="/cadastroUsuario"><h2>Locais de plantação</h2></Link>
-      <Link href="/usuarios/cadastrarUsuario"><h2>Usuários</h2></Link>
-      <Link href="/sobre"><h2>Sobre</h2></Link>
-      <Link href="/home"><h2>Perfil</h2></Link>
-      <Link href="/home"><h2>Notificação</h2></Link>
-      <Link href="/home"><h2>Logout</h2></Link>
+      
+              <Link href="/plantar" className={styleBar.h2Tit}><h2 >Plantar</h2></Link>
+              <Link href="/home" className={styleBar.h2Tit}><h2 >Árvores</h2></Link>
+              <Link href="/cadastroUsuario" className={styleBar.h2Tit}><h2 >Locais de plantação</h2></Link>
+              <Link href="/usuarios/cadastrarUsuario" className={styleBar.h2Tit}><h2 >Usuários</h2></Link>
+              <Link href="/sobre" className={styleBar.h2Tit}><h2 >Sobre</h2></Link>
+              
      
-    </div>
+        </div>
+        <div className={styleBar.opcoesMenu2}>
+              <Link href="/home" className={styleBar.h2Tit}><h2>Perfil</h2></Link>
+              <Link href="/home" className={styleBar.h2Tit}><h2>Notificação</h2></Link>
+              <Link href="/home" className={styleBar.h2Tit}><h2>Logout</h2></Link>
+        </div>
         </div>
         </>
       
