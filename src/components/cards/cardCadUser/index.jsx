1147022c9190345b1a00ac15bd0d2c2baaddcace
Johthema <mauriciojodi@gmail.com>
2023-06-16 11:37:38 -0400

    Atualiza versao

--- src/components/cards/cardCadUser/index.jsx
+++ src/components/cards/cardCadUser/index.jsx
@@ -4,7 +4,9 @@ import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Style from './card.module.css'
+import Style from './card.module.css';
+import Form from 'react-bootstrap/Form';
+
 
 function LeftTabsExample() {
   return (
@@ -26,10 +28,48 @@ function LeftTabsExample() {
             <Card >
                 <Card.Header>Dados Pessoa física</Card.Header>
                 <Card.Body>
-                    <Card.Title>Special title treatment</Card.Title>
+                    {/* <Card.Title>Special title treatment</Card.Title>
                     <Card.Text>
                     With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
+                    </Card.Text> */}
+
+                <Form>
+                    <Row>
+                        <Col>
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control placeholder="Nome" />
+                        </Col>
+                        <Col>
+                        <Form.Label>Sobrenome</Form.Label>
+                        <Form.Control placeholder="Sobrenome" />
+                        </Col>
+                    </Row>
+                    <Row>
+                    <Form.Group className="mb-3" controlId="formGroupEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    </Row>
+                    <Row>
+                    <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>Papel</Form.Label>
+                    <Form.Select >
+                        <option value="1">Administrador</option>
+                        <option value="2">Opção2</option>
+                        <option value="3">Opção3</option>
+                    </Form.Select>
+                    </Form.Group>
+                    </Row>
+                    <br/>
+                    </Form>
+
+
+
+
                     <Button className={Style.BotaoCad}>Salvar</Button>
                 </Card.Body>
             </Card>
