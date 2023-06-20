15168e2776f570604dc5ea680746d809d67b192e
Johthema <mauriciojodi@gmail.com>
2023-06-20 17:54:57 -0400

    Atualiza versao

--- src/pages/manutencao/index.jsx
+++ src/pages/manutencao/index.jsx
@@ -1,10 +1,13 @@
-import Header from '../../components/header/index'
-
+import Header from '../../components/header/index';
+import Image from 'next/image';
+import Logotip from '../../assets/images/manutencao.gif';
+import Style from '../../pages/manutencao/manutencao.module.css'
 export default function Manutencao(){
     return(
         <>
         <Header></Header>
         <h2>Pagina "Manutenção" em construção</h2>
+        <Image src={Logotip}  className={Style.imagemFolha}/>
         </>
     )
 }
