1e4d4e028b7aac9ad23cb5fa735825d1812093e7
Johthema <mauriciojodi@gmail.com>
2023-06-22 17:53:07 -0400

    Atualiza versao

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ export default function NotFound (){
         <h1 className={Style.titulo}>Erro 404! </h1>
         <h1 className={Style.subtitulo}>Desculpe! não conseguimos econtrar a página. </h1>
         <Image src={Logotip}  className={Style.imagemFolha}/>
-        <Link href="/" className={Style.botaoVoltar}>Voltar para home</Link>
+        <Link href="/home" className={Style.botaoVoltar}>Voltar para home</Link>
 
         </div>
        
