37b77f154ac1b91f6af25080a9adaa6cf24b3c7d
Johthema <mauriciojodi@gmail.com>
2023-06-22 17:32:32 -0400

    Atualiza versao cad local em andamento

---
+++ src/components/cards/cardCadLocais/index.jsx
@@ -0,0 +1,106 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Style from './cadlocais.module.css'
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+export default function CadastroLocal() {
+  return (
+    <Card className={Style.cardArvore}>
+      <Card.Header>Cadastro do Local</Card.Header>
+      <Card.Body>
+
+        <Form>
+            <Form.Group className="mb-3" controlId="formGrouNome">
+                <Form.Control type="text" placeholder="Nome" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                   <Form.Control as="textarea" rows={3}  placeholder='Descrição'/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupLocalizacao">
+                <Form.Control type="text" placeholder="Localização" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupEndereco">
+                <Form.Control type="text" placeholder="Endereço" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupCdgPostal">
+                <Form.Control type="text" placeholder="Código postal" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupEndereco">
+                <Form.Control type="text" placeholder="Endereço" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupCidade">
+                <Form.Control type="text" placeholder="Cidade" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupPais">
+                <Form.Control type="text" placeholder="Pais" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formGroupHectare">
+                <Form.Control type="text" placeholder="Hectare" />
+            </Form.Group>
+
+            <Card.Text>
+            Quantidade
+            </Card.Text>
+
+            <Row>
+                <Col>
+                <Form.Control type="number" placeholder="Árvores derrubadas" />
+                </Col>
+                <Col>
+                <Form.Control type="number" placeholder="Árvores ja plantadas " />
+                </Col>
+                <Col>
+                <Form.Control type="number" placeholder="Árvores a plantar" />
+                </Col>
+            </Row>
+
+            <Card.Text>
+            Total: Árvores plantadas + árvores a plantar
+            </Card.Text>
+            <Row className={Style.estiloRadio}>
+                <Col>
+                <Form.Check 
+                type="switch"
+                id="custom-switch"
+                label="Possui irrigação"
+            />
+                </Col>
+                <Col>
+                <Form.Check 
+                type="switch"
+                id="custom-switch"
+                label="Possui viveiro"
+            />
+                </Col>
+
+            </Row>
+           
+    <br/>
+                <Row>
+                <Col>
+                <Form.Group controlId="formFile" className="mb-3">
+                    <Form.Control type="file" className={Style.Preco}/>
+                </Form.Group>
+                <Form.Control placeholder="Árvores" className={Style.Preco}/>
+                </Col>
+             
+                </Row>
+        </Form>
+       
+
+
+        <br/>
+        {/* <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text> */}
+        <Button variant="primary">Salvar</Button>
+      </Card.Body>
+      {/* <Card.Footer className="text-muted">2 days ago</Card.Footer> */}
+    </Card>
+  );
+}  
--- src/pages/locaisPlantacao/cadastrarLocal/index.jsx
+++ src/pages/locaisPlantacao/cadastrarLocal/index.jsx
@@ -1,12 +1,10 @@
-import Header from '../../../components/header/index'
-
+import Header from '../../../components/header/index';
+import CardLocacao from '../../../components/cards/cardCadLocais/index'
 export default function CadastrarLocal(){
     return(
         <div>
             <Header></Header>
-            <h1>
-                Tela de cadastro de local em construção!
-            </h1>
+            <CardLocacao/>
         </div>
     )
 }
