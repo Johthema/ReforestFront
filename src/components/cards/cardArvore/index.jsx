bcba8fabdd5b01a4bc2ba02cacea5a3f18d4189c
Johthema <mauriciojodi@gmail.com>
2023-06-23 13:49:21 -0400

    Atualiza versao card arvore

--- src/components/cards/cardArvore/index.jsx
+++ src/components/cards/cardArvore/index.jsx
@@ -5,6 +5,8 @@ import {FaUserCircle, FaBell, FaDoorOpen, FaCog, FaChartLine, FaHistory,
     FaPlus, FaEnvira, FaMapMarkerAlt, FaUserFriends, FaEdit, FaRegWindowClose } from "react-icons/fa";
 import CloseButton from 'react-bootstrap/CloseButton';
 import Form from 'react-bootstrap/Form';
+import Image from 'next/image';
+import ImgArvore from '../../../assets/images/arvore_1.jpg';
 
 export default function CardArvore() {
   return (
@@ -25,9 +27,10 @@ export default function CardArvore() {
       
       <Card.Body>
         <Card.Title>Tectona grandis</Card.Title>
-        <Card.Text>
+        <Image src={ImgArvore} className={Style.imgArvore} />
+        {/* <Card.Text>
           With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
+        </Card.Text> */}
         {/* <Button variant="primary">Go somewhere</Button> */}
       </Card.Body>
       <Card.Footer className="text-muted">
