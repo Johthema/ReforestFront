17ac15c43a99080427e98a1057c5e90dba05dc9a
Johthema <mauriciojodi@gmail.com>
2023-06-27 16:25:57 -0400

    Atualiza versao lista de usuario em andamento

---
+++ src/pages/service/index.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+
+const URL_API=  "http://192.168.0.249:3001/api/users";
+
+export default function ListaUs() {
+
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null)
+  
+  //Primeiro carregamengto para saber se esta tudo certo
+    const fecthAllData = async ()=> {
+      try {
+       
+        setLoading (true)
+        const response = await fetch(URL_API) //por padrão o fetch ja utiliza o GET
+        const data = await response.json()
+
+        if (!data) 
+          throw 'problema na requisição' //Aqui será tratado o erro de requisição. Porém é melhor tratar pelo status(200, 400, 500)
+        setData(data)
+  
+    //Iniciando a estrutura da requisição
+  
+      } catch (error) {
+        console.log(error)
+      } finally{
+        setLoading (false)
+      }
+  
+    }
+  
+    //useEffect Lida com o ciclo de vida da aplicação para não ficar em loop infinito
+    useEffect(()=>{
+      fecthAllData();
+  
+    },[])
+  
+    
+    return (
+   
+    <>
+  {/* Primeiro carregamento será o loadingo para saber se existe algo em date */}
+    <h2>Requisicao com api</h2>
+      {loading && !data &&
+      <h3>Carregando informações...</h3>
+      }
+  
+  {/* Segundo carregamento será para listar cada item existente no array data pelo metodo map */}
+      {data && data.map((item) => (
+        <p key={item._id}> {item.name}</p>
+        
+      ))}
+  
+    </>
+    )
+    
+  }
+  
+  
--- src/pages/usuarios/listaUsuario/index.jsx
+++ src/pages/usuarios/listaUsuario/index.jsx
@@ -1,22 +1,67 @@
 import Header from '../../../components/header/index'
+import { useEffect, useState } from 'react'
+import Style from './listauser.module.css'
+const URL_API=  "http://192.168.0.249:3001/api/users";
+
 
 export default function ListarUsuario(){
+   
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState(null)
+  
+  //Primeiro carregamengto para saber se esta tudo certo
+    const fecthAllData = async ()=> {
+      try {
+       
+        setLoading (true)
+        const response = await fetch(URL_API) //por padrão o fetch ja utiliza o GET
+        const data = await response.json()
+
+        if (!data) 
+          throw 'problema na requisição' //Aqui será tratado o erro de requisição. Porém é melhor tratar pelo status(200, 400, 500)
+        setData(data)
+  
+    //Iniciando a estrutura da requisição
+  
+      } catch (error) {
+        console.log(error)
+      } finally{
+        setLoading (false)
+      }
+  
+    }
+  
+    //useEffect Lida com o ciclo de vida da aplicação para não ficar em loop infinito
+    useEffect(()=>{
+      fecthAllData();
+  
+    },[])
+
+
     return(
         <div>
             <Header></Header>
             <h1>
                 Tela de lista de usuários em construção!
             </h1>
+
             <ul>
-                <li>
-                    <h3>Usuario 1</h3>
-                </li>
-                <li>
-                    <h3>Usuario 2</h3>
-                </li>
-                <li>
-                    <h3>Usuario 3</h3>
-                </li>
+                    {/* Primeiro carregamento será o loadingo para saber se existe algo em data */}
+                {loading && !data &&
+                <h3>Carregando informações...</h3>
+                }
+            
+                    {/* Segundo carregamento será para listar cada item existente no array data pelo metodo map */}
+                {data && data.map((item) => (
+                    <ul className={Style.listaItemUsuario}>
+                        <li className={Style.itemUsuario}>
+                        <h2 key={item._id} className={Style.FontUsuario}> {item.name}</h2>
+                        </li>
+                    </ul>
+ 
+                    
+                ))}
+             
             </ul>
         </div>
     )
