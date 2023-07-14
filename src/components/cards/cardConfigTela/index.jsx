c5eb1f2ecc4ad25a3f50514ca350ca9d6e502033
Johthema <mauriciojodi@gmail.com>
2023-07-14 13:37:27 -0400

    Atualiza versao

--- src/components/cards/cardConfigTela/index.jsx
+++ src/components/cards/cardConfigTela/index.jsx
@@ -14,7 +14,7 @@ export default function Tela(){
 
         <Card style={{ width: '18rem' }} className={Style.Card1}>
        
-        <Card.Body className={Style.Corpo}>
+        <Card.Body className={Style.Corpo1}>
        
             <Card.Title className={Style.Titulo1}>Clara</Card.Title>
             <Card.Text>
@@ -34,26 +34,26 @@ export default function Tela(){
         </Card>
 
 
-
-    <Card style={{ width: '18rem' }} className={Style.Card2}>
-      
-      <Card.Body>
-        <Card.Title>Escura</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-      <ListGroup.Item className={Style.Item3}>Alerta</ListGroup.Item>
-        <ListGroup.Item className={Style.Item4}>Sucesso!</ListGroup.Item>
-        <ListGroup.Item className={Style.Item5}>Carregando..</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Links</Card.Link>
-        <Button variant="primary">Botão</Button>
-      </Card.Body>
-    </Card>
+        <Card style={{ width: '18rem' }} className={Style.Card2}>
+       
+        <Card.Body className={Style.Corpo2}>
+       
+            <Card.Title className={Style.Titulo2}>Clara</Card.Title>
+            <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+            </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+            <ListGroup.Item className={Style.Item4}>Alerta</ListGroup.Item>
+            <ListGroup.Item className={Style.Item5}>Sucesso!</ListGroup.Item>
+            <ListGroup.Item className={Style.Item6}>Carregando..</ListGroup.Item>
+        </ListGroup>
+        <Card.Body>
+            <Card.Link className={Style.Link1}>Links </Card.Link>
+            <Button variant="primary">Botão</Button>
+        </Card.Body>
+        </Card>
 
 
 
