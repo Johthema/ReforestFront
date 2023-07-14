bf16674d3248d69a079e32fc862281118d946bda
Johthema <mauriciojodi@gmail.com>
2023-07-14 10:30:32 -0400

    Atualiza versao configuracao

--- src/pages/configuracao/index.jsx
+++ src/pages/configuracao/index.jsx
@@ -3,30 +3,37 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Footer from '../../components/footer/index'
 import CardUsuario from '../../components/cards/cardCadUser/index';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Style from './configure.module.css'
 
 export default function Configuracao(){
     return(
         <>
         <Header></Header>
+        <Breadcrumb className={Style.migalhas}>
+                <Breadcrumb.Item href="/home">Home</Breadcrumb.Item>
+                
+                <Breadcrumb.Item active>Configuração.</Breadcrumb.Item>
+            </Breadcrumb>
 
         <Tabs
-        defaultActiveKey="profile"
+        defaultActiveKey="info"
         id="justify-tab-example"
         className="mb-3"
         justify
         >
-        <Tab eventKey="home" title="Informações pessoais">
-            Informações pessoais
+        <Tab eventKey="info" title="Informações pessoais">
+         
             <CardUsuario /> 
         </Tab>
-        <Tab eventKey="profile" title="Permissões">
-            Permissões
+        <Tab eventKey="perm" title="Permissões">
+          
         </Tab>
-        <Tab eventKey="longer-tab" title="Tela">
-            Tela
+        <Tab eventKey="tela" title="Tela">
+           
         </Tab>
-        <Tab eventKey="contact" title="Senha" >
-            Senha
+        <Tab eventKey="password" title="Senha" >
+        
         </Tab>
         </Tabs>
 
