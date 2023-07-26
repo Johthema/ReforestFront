d0f9df69670a34a64228b5d36e81565a4320f75c
Johthema <mauriciojodi@gmail.com>
2023-07-26 17:42:40 -0400

    Atualiza versao

--- src/pages/arvores/listarArvores/index.jsx
+++ src/pages/arvores/listarArvores/index.jsx
@@ -1,5 +1,5 @@
 import Header from '../../../components/header/index'
-
+import Arvores from '../../../components/cards/cardArvore/index'
 export default function ListarArvore(){
     return(
         <div>
@@ -7,6 +7,11 @@ export default function ListarArvore(){
             <h1>
                 Tela de lista de árvore em construção!
             </h1>
+        <Arvores/>
+        <hr/>
+        <h1>
+                Lista em construção!
+        </h1>
         </div>
     )
 }
