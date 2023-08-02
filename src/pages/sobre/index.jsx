cf588397dcb99d0dface98e1b159320d06d9e027
Johthema <mauriciojodi@gmail.com>
2023-08-02 17:23:48 -0400

    Atualiza versao sobre

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -1,10 +1,24 @@
-import Header from '../../components/header/index'
+import Header from '../../components/header/index';
+import Style from './sobre.module.css';
+import Image from 'next/image';
+import Perfil from '../../assets/images/fotoperfil/fotoper.jpg';
+
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 export default function Sobre(){
     return(
         <>
         <Header></Header>
-        <h2>Pagina "Sobre" em construção</h2>
+        <div className={Style.DivHeader}>
+
+        <Image src={Perfil} roundedCircle className={Style.FotoPerfil} />
+           
+        
+
+        </div>
+       
         </>
     )
 }
