c5dbfd7ec6c659f6c5a1b10e3e6345319a4187ad
Johthema <mauriciojodi@gmail.com>
2023-08-03 17:45:41 -0400

    Atualiza versao

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,7 +2,10 @@ import Style from './footer.module.css'
 export default function Footer(){
     return(
         <div className={Style.fundoRodape}>
-            <h1 className={Style.Titulo}>Rodape</h1>
+            {/* <h1 className={Style.Titulo}>Rodape</h1> */}
+            <h5 className={Style.Titulo}>Termos de Uso</h5>
+            <h5 className={Style.Titulo}>Política de Privacidade</h5>
+            <h5 className={Style.Titulo}>Contato</h5>
         </div>
     )
 }
