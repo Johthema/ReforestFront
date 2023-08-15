7bb9cb22d9dcae3639e28d64969d8268ab3a376b
Johthema <mauriciojodi@gmail.com>
2023-08-15 14:21:19 -0400

    Atualiza versao- lista de locais

--- src/pages/locaisPlantacao/listaLocais/index.jsx
+++ src/pages/locaisPlantacao/listaLocais/index.jsx
@@ -1,13 +1,55 @@
-import Header from '../../../components/header/index'
-import Arvores from '../../../components/cards/cardArvore/index'
+import Header from '../../../components/header/index';
+import Arvores from '../../../components/cards/cardArvore/index';
+import Style from './listalocais.module.css';
+import {useState, useEffect} from 'react';
+
+
+
 export default function ListarLocal(){
+    const URL_API = process.env.NEXT_PUBLIC_API_URL+"plantingPlace";
+
+    const [reloadCount, setReloadCount] = useState(0);
+    const [dataCat, setData] = useState([]);
+    const [name, setNome] = useState('');
+  //Primeiro carregamengto para saber se esta tudo certo
+  const fecthAllData = async () => {
+    try {
+
+      //setLoading(true)
+      const response = await fetch(URL_API) //por padrão o fetch ja utiliza o GET
+      const dataCat = await response.json()
+
+      if (!dataCat)
+        throw 'problema na requisição' //Aqui será tratado o erro de requisição. Porém é melhor tratar pelo status(200, 400, 500)
+      setData(dataCat)
+
+      setNome(dataCat.name)
+      console.log("Lista de locais: ",dataCat)
+      //Iniciando a estrutura da requisição
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+    //   setLoading(false)
+    }
+
+  }
+
+  useEffect(() => {
+    fecthAllData();
+
+  }, [reloadCount]);
+
+
     return(
         <div>
             <Header></Header>
             <h1>
-                Tela de lista de locais em construção!
+                Lista de locais de plantação em atividade
             </h1>
-            <Arvores/>
+           <div>
+
+           </div>
 
         </div>
     )
