5112cd6f3b88fd34f69b78d1eb41766758655346
Johthema <mauriciojodi@gmail.com>
2023-08-17 16:53:20 -0400

    Atualiza versao

--- src/pages/locaisPlantacao/cadastrarLocal/cardCadLocalEdit/index.jsx
+++ src/pages/locaisPlantacao/cadastrarLocal/cardCadLocalEdit/index.jsx
@@ -324,7 +324,7 @@ export default function EditarLocal({handleShowEdit}) {
                 onChange={onChangeNursery}
             />
                 </Col>
-
+ 
             </Row>
            
     <br/>
