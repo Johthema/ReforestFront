a1e2ad4f483cb35a8e9a2a8623687038d00da1eb
Johthema <mauriciojodi@gmail.com>
2023-08-21 09:27:08 -0400

    Atualiza versao

---
+++ src/pages/landingpage/index.jsx
@@ -0,0 +1,8 @@
+import Style from './landing.module.css'
+export default function LandingPage(){
+    return(
+        <div className={Style.DivFundoladingpage}>
+        <h2>Pagina de landing page!</h2>
+        </div>
+    )
+}
