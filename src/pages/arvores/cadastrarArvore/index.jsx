2233dc04df3b5923e84b470f99e7325be9c2e0f7
Johthema <mauriciojodi@gmail.com>
2023-08-22 11:29:58 -0400

    Atualiza versao

--- src/pages/arvores/cadastrarArvore/index.jsx
+++ src/pages/arvores/cadastrarArvore/index.jsx
@@ -1,17 +1,22 @@
 import Header from '../../../components/header/index';
 import CardArvore from '../../../components/cards/cardCadArvore/index';
-import Style from './cadarvore.module.css'
+import Style from './cadarvore.module.css';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Footer from '../../../components/footer';
 
 export default function CadastrarArvore(){
     return(
         <>
         <Header></Header>
-        <div className={Style.divFundoCad}>
+        <div className={Style.divFundoCad}> 
+        <Breadcrumb className={Style.migalhas}>
+        <Breadcrumb.Item href="/home">Home  </Breadcrumb.Item>   
+        <Breadcrumb.Item active>Cadastro de Árvore.</Breadcrumb.Item>
+        </Breadcrumb>
         <CardArvore/>
         </div>
             
-           
-                
+           <Footer/>                
            
             
         </>
