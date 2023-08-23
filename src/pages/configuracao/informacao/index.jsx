ddc59dfc950c95730cd114e8bf94cdf06893adb9
Johthema <mauriciojodi@gmail.com>
2023-08-23 16:25:26 -0400

    Atualiza versao editar perfil user em andamento

--- src/pages/configuracao/informacao/index.jsx
+++ src/pages/configuracao/informacao/index.jsx
@@ -1,9 +1,35 @@
 import CardUsuario from '../../../components/cards/cardCadUser/index';
-
+import CardUsuarioEdit from '../../../components/cards/cardCadUser/cardCadEdit/index';
+import { useEffect, useState } from 'react';
 export default function Informacao(){
+    
+    const [showEdit, setShowEdit] = useState(false);
+  
+    const [dadosEditar, setDadosEditar] = useState()
+    // const [dadosEditar, setDadosEditar] = useState([
+    //     idEditUser,
+    //     'PF'
+    // ]);
+
+    const handleShowEdit = () =>{
+        // console.log("o id a passar agora: ", idUser)
+        setShowEdit(true);
+        const dadosEditar = [localStorage.getItem("idUs"), 'PF']
+        setDadosEditar(dadosEditar);
+      }
+
+      useEffect(() => {
+        handleShowEdit(dadosEditar)
+      },[])
+    
+    
     return(
+        
         <>
-            <CardUsuario/> 
+        {showEdit &&
+         <CardUsuarioEdit handleShowEdit={dadosEditar}/>
+         }
+             
         </>
     )
 }
