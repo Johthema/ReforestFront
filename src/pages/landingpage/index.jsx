87173d0adfb60f09cc35d4dd7156bc1dc6348511
Johthema <mauriciojodi@gmail.com>
2023-08-25 16:33:03 -0400

    Atualiza versao landing page em andamento

--- src/pages/landingpage/index.jsx
+++ src/pages/landingpage/index.jsx
@@ -2,6 +2,7 @@ import Style from './landing.module.css';
 import Image from 'next/image';
 import Button from 'react-bootstrap/Button';
 import Foto1 from '../../assets/images/fotopage1.png';
+import Logo from '../../assets/images/logo.png';
 // import Foto2 from '../../assets/images/fotopage2.jpg';
 // import Foto3 from '../../assets/images/fotopage3.jpg';
 // import Foto4 from '../../assets/images/fotopage4.jpg';
@@ -10,7 +11,13 @@ export default function LandingPage(){
     return(
         <>
         <div className={Style.DivFundoladingpage}>
-            <div className={Style.DivHeaderPage}>
+           
+            <div >
+                <div className={Style.menuLogin}>
+                    <div className={Style.btnItem1}>Cadastre-se</div>
+                    <div className={Style.btnItem2}>Login</div>
+                </div>
+                <div className={Style.DivHeaderPage}>
                 <div className={Style.DivTextoPage}>
                     <div>
                         <h4>
@@ -25,13 +32,19 @@ export default function LandingPage(){
                         </h4>
                     </div>
                     <div className={Style.botaoPlantar}>
-                    Plantar suas árvores
+                    Plante sua árvore
                     </div>
                 
                 </div>
             <Image src={Foto1} className={Style.Estilofoto1} alt="" />
             </div>
-       
+
+
+            <div>
+            <Image src={Logo} className={Style.EstiloLogo} alt="" />
+            </div>
+           
+            </div>
         </div>
         </>
     )
