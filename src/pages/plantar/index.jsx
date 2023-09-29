1304128abd09287d407f7d784d54af460dbf6147
Johthema <mauriciojodi@gmail.com>
2023-09-29 09:15:39 -0400

    Atualiza versao

--- src/pages/plantar/index.jsx
+++ src/pages/plantar/index.jsx
@@ -306,7 +306,7 @@ const items = [];
       <div className={Style.divFundoPlantar}>
 
         <div className={Style.Coluna1}>
-<div className={Style.divTituloPassos}><h5>Passo 1 - Escolha um local de plantação</h5></div>
+<div className={Style.divTituloPassos}><h5><span className={Style.PassosEstilo}>1</span> - Escolha um local de plantação</h5></div>
 
 <AliceCarousel 
         mouseTracking
