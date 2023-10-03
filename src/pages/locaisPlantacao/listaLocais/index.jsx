7693a9d26a05f1d179ad46806f2f3a26ef6270e5
Johthema <mauriciojodi@gmail.com>
2023-10-03 14:52:30 -0400

    Atualiza versao

--- src/pages/locaisPlantacao/listaLocais/index.jsx
+++ src/pages/locaisPlantacao/listaLocais/index.jsx
@@ -359,7 +359,7 @@ const onChangeOrdem=(ordem) =>{
                           </Card.Text>
                           {/* <Button variant="primary">Go somewhere</Button> */}
                         </Card.Body>
-                        <Card.Footer className="text-muted">
+                        <Card.Footer className={Style.textmuted}>
                           {/* <div className={Style.footerCard}>
                            
                           </div> */}
