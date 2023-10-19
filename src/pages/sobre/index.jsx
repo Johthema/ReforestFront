017ec2bb48e74ed1932602a50a8d832ce4465d9f
Johthema <mauriciojodi@gmail.com>
2023-10-19 15:54:54 -0400

    Atualiza versao sobre em andamento

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -8,12 +8,39 @@ export default function Sobre (){
             <Header/>
             <div className={Style.barraCabecalho}>
                 <div className={Style.divTitleSubTit}>
-                    <h1 className={Style.legendaCabecalho}><b>Sobre a</b> Reflorest</h1>
+                    <h1 className={Style.legendaCabecalho}><b>Sobre a</b> Reforest</h1>
                     <h5 className={Style.subtituloCabecalho}>Tecnologia Verde: Um Sistema Web para o Futuro da Reflorestação</h5>
                   
                     <Button className={Style.botaoPlantar} href="/plantar">Plantar agora</Button>
                 </div>
               
+            </div>
+            <div className={Style.divPropMetd}>
+                <div className={Style.divProposta}>
+                    <h2>Nossa proposta</h2>
+                    <p> Estabelecer um programa de reflorestamento para combater a degradação ambiental,
+                         promover a conservação da biodiversidade e reduzir as emissões de carbono em uma determinada área.</p>
+                </div>
+                <div className={Style.divMetodo}>
+                    <h2>Nosso método</h2>
+                    <p> Nosso metodo é Estabelecer um programa de reflorestamento para combater a degradação ambiental,
+                         promover a conservação da biodiversidade e reduzir as emissões de carbono em uma determinada área.</p>
+                </div>
+            </div>
+            <div className={Style.divProvaSocial}>
+                <div className={Style.divTituloLugaresPlant}>
+                    <h3>Alguns lugares de plantação</h3>
+                </div>
+                <div className={Style.divProva1}>
+                    <div className={Style.divIcone}></div>
+                    <div className={Style.Prova1}>
+                    </div>   
+                    <div className={Style.divImagem}></div>
+                       
+                </div>
+                
+                
+
             </div>
             <Footer/>
         </div>
