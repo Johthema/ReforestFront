393a922d06a8c9fd3df04d0aafe0e29ac85844be
Johthema <mauriciojodi@gmail.com>
2023-10-19 13:21:15 -0400

    Atualiza versao sobre em andamento

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -1,12 +1,19 @@
 import Style from './sobre.module.css';
 import Header from '../../components/header/index';
 import Footer from '../../components/footer/index';
+import Button from 'react-bootstrap/Button';
 export default function Sobre (){
     return(
         <div className={Style.barraCabelho}>
             <Header/>
             <div className={Style.barraCabecalho}>
-                <h1 className={Style.legendaCabecalho}><b>Juntos pela Natureza:</b> Plante a Semente da Esperança e Ajude a Reflorestar o Nosso Futuro!</h1>
+                <div className={Style.divTitleSubTit}>
+                    <h1 className={Style.legendaCabecalho}><b>Sobre a</b> Reflorest</h1>
+                    <h5 className={Style.subtituloCabecalho}>Tecnologia Verde: Um Sistema Web para o Futuro da Reflorestação</h5>
+                  
+                    <Button className={Style.botaoPlantar} href="/plantar">Plantar agora</Button>
+                </div>
+              
             </div>
             <Footer/>
         </div>
